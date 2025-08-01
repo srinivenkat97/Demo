c1c71379c40fc345b6825d69bce962db48e34c51
srinivenkat97 <srinivasanvenkat54@gmail.com>
2025-08-01 15:18:09 +0530

    push 16

--- tests/exchangerates.spec.js
+++ tests/exchangerates.spec.js
@@ -64,11 +64,11 @@ test('Extract Exchange Rates and Send to Bitrix24 and Oracle DB', async ({ page 
 
 
 const config = {
-      user: process.secrets.SQL_USER,
-      password: process.secrets.SQL_PASSWORD,
-      server: process.secrets.SQL_SERVER,
-      port: process.secrets.SQL_PORT,
-      database: process.secrets.SQL_DATABASE,
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      server: process.env.SQL_SERVER,
+      port: process.env.SQL_PORT,
+      database: process.env.SQL_DATABASE,
       options: {
         encrypt: true,
         trustServerCertificate: false,
